07c55b0345cdb5450a048c7147a96ee35eb39450
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-12-06 16:40:24 +0800

    Hide tweet modal after submit, hide body label inside modal, put Tweet on the right side

--- client/templates/posts/posts.js
+++ client/templates/posts/posts.js
@@ -3,3 +3,19 @@ Template.posts.helpers({
 		return Posts.find({}, {sort: {createdAt: -1}});
 	}
 });
+
+// function to close the modal after tweet has been saved
+var insertPostFormHook = {
+    onSuccess: function(operation, result) {
+        if(result){
+
+            console.log(operation);	// should be `insert`
+            console.log(result); 	// return saved tweet `_id`
+
+            $('#postModal').modal('toggle');
+        }
+    }
+}
+
+// register above function to autoForm
+AutoForm.addHooks('insertPostForm', insertPostFormHook);
