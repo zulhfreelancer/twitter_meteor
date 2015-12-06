8005c9aab41fc6ca23b51263b4484a675c85a47e
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-12-06 17:08:58 +0800

    Apply MomentJS at the createdAt so that we can see 'X seconds ago' / 'X minutes ago'

--- client/common.js
+++ client/common.js
@@ -20,3 +20,14 @@ Template.registerHelper('getProfileImage', function(userId){
 	var imgUrl = UserImages.findOne({userId: userId}).image;
 	return imgUrl;
 });
+
+// global function to display tweet creation time using MomentJS
+Template.registerHelper('getMomentTime', function(postId){
+	// find that post using given postId (passed from the posts.html)
+	// from the post, grab the createdAt value
+	var tweetDate 	= Posts.findOne({_id: postId}).createdAt;
+	// apply MomentJS based on current time
+	var momentTime 	= moment(tweetDate).fromNow();
+	// give back to the template
+	return momentTime;
+});
